1bc6e71c344ce785468afd4e8ef8677be8d3f67a
Miguel Caballero <cp0251963@gmail.com>
2021-06-30 00:14:22 -0400

    updated

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const controllers = require('./controllers');
 
 app.use(Express.json());
 
+app.use(require('./middleware/headers'));
+
 app.use('/workoutlog', controllers.workoutlogcontroller);
 
 //app.use(require('./middleware/validate-jwt'));
@@ -16,8 +18,8 @@ dbConnection
   .authenticate()
   .then(() => dbConnection.sync())
   .then(() => {
-    app.listen(3000, () => {
-      console.log(`[Server]: App is lstening on 3000.`);
+    app.listen(8080, () => {
+      console.log(`[Server]: App is lstening on 8080.`);
     });
   })
   .catch((err) => {
